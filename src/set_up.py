bbecf2cb509b2e234eedfe67cb2728f52f1a28f5
Na1an <yuchenbai@hotmail.com>
2022-04-28 16:57:49 +0200

    add set_tup

---
+++ src/set_up.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='None yet',
+        version='0.1',
+        packages=[''],
+        url='',
+        license='',
+        author='Yuchen BAI',
+        author_email='yuchen.bai@inria.fr',
+        description=''
+)
