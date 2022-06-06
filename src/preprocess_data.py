4c67cc029b81d36c3f844e8192a20c2de77d2e64
Na1an <yuchenbai@hotmail.com>
2022-06-06 23:37:25 +0200

    looks good, rotation is uploaded

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -255,7 +255,7 @@ def prepare_dataset(data, coords_sw, grid_size, voxel_size, global_height, voxel
 
             # (4) voxelization
             key_points_in_voxel, nb_points_per_voxel, voxel = voxel_grid_sample(local_points, voxel_size, voxel_sample_mode)
-            voxel_skeleton_cuboid[w_nb] = voxel
+            voxel_skeleton_cuboid[count_voxel_skeleton] = voxel
             #visualize_voxel_key_points(voxel, nb_points_per_voxel, "TLS voxelized data")
 
             # (5) centralization in (x y z) thress axis by the center of voxels
@@ -280,16 +280,40 @@ def prepare_dataset(data, coords_sw, grid_size, voxel_size, global_height, voxel
             tmp_samples = [local_points_tmp[sample_size*i_t:sample_size*(i_t+1)] for i_t in range(tmp_nb_sample)]
             [samples.append(item) for item in tmp_samples]
 
+            
             # data augmentation
             rotation = Rotation.from_euler('z', [90, 180, 270], degrees=True)
             for angle in range(3):
                 print(">>> [data augmentation - sample rotated:{}°]".format((angle+1)*90))
-                rotation[angle]
+                #rotation[angle]
                 local_points_tmp = local_points.copy()
+                local_points_tmp[:,:3] = local_points_tmp[:,:3] - np.array([grid_size/2, grid_size/2, global_height/2])
+                local_points_tmp[:,:3] = rotation[angle].apply(local_points_tmp[:,:3])
+                local_points_tmp[:,:3] = local_points_tmp[:,:3] + np.array([grid_size/2, grid_size/2, global_height/2])
+
+                # (4) voxelization
+                key_points_in_voxel, nb_points_per_voxel, voxel = voxel_grid_sample(local_points_tmp, voxel_size, voxel_sample_mode)
+                voxel_skeleton_cuboid[count_voxel_skeleton] = voxel
+                #visualize_voxel_key_points(voxel, nb_points_per_voxel, "TLS voxelized data")
+
                 local_points_tmp[:,:3] = local_points_tmp[:,:3] - np.array([grid_size/2, grid_size/2, global_height/2])
                 local_points_tmp[:,:2] = local_points_tmp[:,:2]/grid_size
                 local_points_tmp[:,2] = local_points_tmp[:,2]/global_height
-            
+                
+                # set sample_cuboid_index
+                for i_s in range(nb_sample, nb_sample+tmp_nb_sample):
+                    sample_cuboid_index[i_s] = count_voxel_skeleton
+                count_voxel_skeleton = count_voxel_skeleton + 1
+                nb_sample = nb_sample + tmp_nb_sample
+
+                if detail:
+                    print(">>> tmp_nb_sample={}, nb_sample+tmp={}".format(tmp_nb_sample, nb_sample))
+                
+                # set samples
+                np.random.shuffle(local_points_tmp)
+                tmp_samples = [local_points_tmp[sample_size*i_t:sample_size*(i_t+1)] for i_t in range(tmp_nb_sample)]
+                [samples.append(item) for item in tmp_samples]
+                
             # sliding_window added 1
             w_nb = w_nb + 1
                 
