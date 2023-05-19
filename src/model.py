be1f1cfe4c17e1ec1ee0aba8e1a929860fc32522
Na1an <yuchenbai@hotmail.com>
2023-05-19 15:12:22 +0200

    model revised

--- src/model.py
+++ src/model.py
@@ -5,15 +5,10 @@ import numpy as np
 from torch.autograd import Variable
 from pointnet2_utils import PointNetSetAbstraction,PointNetFeaturePropagation
 
-###############
-# version ier #
-###############
-
 # PointNet++
 class Pointnet_plus(nn.Module):
     def __init__(self, num_classes):
         super(Pointnet_plus, self).__init__()
-
         # 3+12 : (x,y,z) + (verticality, specificity, pca1, linearity) * 3
         self.sa1 = PointNetSetAbstraction(1024, 0.1, 32, 15 + 3, [32, 32, 64], False)
         self.sa2 = PointNetSetAbstraction(256, 0.2, 32, 64 + 3, [64, 64, 128], False)
@@ -28,21 +23,21 @@ class Pointnet_plus(nn.Module):
         self.drop1 = nn.Dropout(0.3)
         self.conv2 = nn.Conv1d(128, num_classes, 1)
 
-    def forward(self, xyz):
-        l0_points = xyz
-        l0_xyz = xyz[:,:3,:]
+    def forward(self, points_for_pointnet):
+        '''
+        ps_fs: points + features.
+        '''
+        l0_ps_fs = points_for_pointnet[:,:3,:]
+        l1_ps_fs, l1_points = self.sa1(l0_ps_fs, points_for_pointnet)
+        l2_ps_fs, l2_points = self.sa2(l1_ps_fs, l1_points)
+        l3_ps_fs, l3_points = self.sa3(l2_ps_fs, l2_points)
+        l4_ps_fs, l4_points = self.sa4(l3_ps_fs, l3_points)
+        l3_points = self.fp4(l3_ps_fs, l4_ps_fs, l3_points, l4_points)
+        l2_points = self.fp3(l2_ps_fs, l3_ps_fs, l2_points, l3_points)
+        l1_points = self.fp2(l1_ps_fs, l2_ps_fs, l1_points, l2_points)
+        features = self.fp1(l0_ps_fs, l1_ps_fs, None, l1_points)
 
-        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
-        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
-        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
-        l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
-
-        l3_points = self.fp4(l3_xyz, l4_xyz, l3_points, l4_points)
-        l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)
-        l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)
-        l0_points = self.fp1(l0_xyz, l1_xyz, None, l1_points)
-
-        return l0_points
+        return features
 
 # MPVCNN: multi-scale Point Voxel CNN
 class PointWiseModel(nn.Module):
@@ -58,64 +53,18 @@ class PointWiseModel(nn.Module):
         '''
         super().__init__()
         # define architecture here
-        #self.actvn = nn.ReLU()
         self.actvn = nn.ELU()
         self.maxpool = nn.MaxPool3d(2)
         self.num_classes = num_classes
         self.show_net_shape = False
         
-        #nn.Conv3d(N batch_size, Cin 输入图像通道数, D深度/高度, H图像高, W图像宽, padding=1)
-        # 给voxel skeleton准备的net，只为提取feature
-        #class torch.nn.Conv3d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
-        """ 
-        in_channels(int) – 输入信号的通道，就是输入中每帧图像的通道数
-        out_channels(int) – 卷积产生的通道，就是输出中每帧图像的通道数
-        kernel_size(int or tuple) - 过滤器的尺寸，假设为(a,b,c)，表示的是过滤器每次处理 a 帧图像，该图像的大小是b x c。
-        stride(int or tuple, optional) - 卷积步长，形状是三维的，假设为(x,y,z)，表示的是三维上的步长是x，在行方向上步长是y，在列方向上步长是z。
-        padding(int or tuple, optional) - 输入的每一条边补充0的层数，形状是三维的，假设是(l,m,n)，表示的是在输入的三维方向前后分别padding l 个全零二维矩阵，在输入的行方向上下分别padding m 个全零行向量，在输入的列方向左右分别padding n 个全零列向量。
-        dilation(int or tuple, optional) – 卷积核元素之间的间距，这个看看空洞卷积就okay了
-        groups(int, optional) – 从输入通道到输出通道的阻塞连接数；没用到，没细看
-        bias(bool, optional) - 如果bias=True，添加偏置；没用到，没细看 
-        """
-
-        '''
-        # voxel-based branch: if-net
-        # kernel_size = 3
-        self.conv_1 = nn.Conv3d(1, 16, 3, padding=1)  # out: 32
-        self.conv_1_1 = nn.Conv3d(16, 32, 3, padding=1)  # out: [32]
-        self.conv_2 = nn.Conv3d(32, 64, 3, padding=1)  # out: 64
-        self.conv_2_1 = nn.Conv3d(64, 64, 3, padding=1)  # out: [64]
-        self.conv_3 = nn.Conv3d(64, 64, 3, padding=1)  # out: 32
-        self.conv_3_1 = nn.Conv3d(64, 64, 3, padding=1)  # out: [64]
-        '''
         # feature_size was setting 3 times for multi-scale learning/multi receptive field
-        # + (3 + 32): intensity added + roughness added + ncr added, 32 is output of pointfeatures ... 
-        # + 128 : output of fc of the pointwise_features
-        # + (128 + 64) : pointnet segmentation output
-        #feature_size = (1 + 32 + 64 + 64) + (3 + 32) + (128)
-        #feature_size = (1 + 32 + 64 + 64) + (3) + (128)
-        #feature_size = 1 + (32 + 64 + 64) + 3 + 32
-        #feature_size = (3 + 32) + (128)
+        # + 128 
         feature_size = 128
-
-        # conditionnal VAE, co-variabale, regression
         self.fc_0 = nn.Conv1d(feature_size, hidden_dim*2, 1)
         self.fc_1 = nn.Conv1d(hidden_dim*2, hidden_dim, 1)
-        #self.fc_2 = nn.Conv1d(hidden_dim*2, hidden_dim, 1)
-        #self.fc_out = nn.Conv1d(hidden_dim, 1, 1) chang  1 to self.num_classes
+        # self.num_classes : the number of class, e.g. 2 for leaf&wood.
         self.fc_out = nn.Conv1d(hidden_dim, self.num_classes, 1)
-
-        '''
-        self.conv1_1_bn = nn.BatchNorm3d(32)
-        self.conv2_1_bn = nn.BatchNorm3d(64)
-        self.conv3_1_bn = nn.BatchNorm3d(64)
-
-        # point_feature_size = 7
-        #self.mlp_0 = nn.Conv1d(7, hidden_dim*2, 1)
-        self.mlp_0 = nn.Conv1d(4, hidden_dim, 1)
-        self.mlp_1 = nn.Conv1d(hidden_dim, hidden_dim, 1)
-        self.mlp_2 = nn.Conv1d(hidden_dim, 32, 1)
-        '''
 
         # point-based branch: PointNet
         self.point_base_model = Pointnet_plus(num_classes=self.num_classes)
@@ -124,20 +73,14 @@ class PointWiseModel(nn.Module):
     def forward(self, points, pointwise_features, v, points_for_pointnet):
         '''
         Args:
-            p: points, a 3d pytorch tensor.
-            pointwise_features: pointwise_features[0] = intensity.
-            v: voxel net.
-            points_for_pointnet: feed this data to poinnet++. [batch_size, coordiantes+features+coordinates_scaled, nb of points] e.g. [4,9,5000] 
+            Args.
         Returns: 
             None.
         '''
-
         features = self.point_base_model(points_for_pointnet)
         net_out = self.actvn(self.fc_0(features))
         net_out = self.actvn(self.fc_1(net_out))
-        #net_out = self.actvn(self.fc_2(net_out))
         net_out = self.fc_out(net_out)
         out = net_out.squeeze(1)
-
         return out
 
